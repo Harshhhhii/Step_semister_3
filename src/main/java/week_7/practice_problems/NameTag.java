8f2be70db6a17053b4fb13081e19ffa57c5dd6e1
Harshitha GV <gvharshitha2007@gmail.com>
2026-09-19 09:53:18 +0530

    Create NameTag.java

---
+++ src/main/java/week_7/practice_problems/NameTag.java
@@ -0,0 +1,14 @@
+public class NameTag {
+    private final String firstName;
+    private final String lastNameInitial;
+
+    public NameTag(String fullName) {
+        String[] parts = fullName.split(" ");
+        this.firstName = parts[0];
+        this.lastNameInitial = parts[1].substring(0, 1) + ".";
+    }
+
+    public String getNickname() {
+        return this.firstName + " " + this.lastNameInitial;
+    }
+}
